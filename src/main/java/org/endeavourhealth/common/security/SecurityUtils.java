aa54d09772a37fe60ee103ee5a8e748a7d6039f0
Richard Collier <ergosoftuk@gmail.com>
2018-04-20 14:13:22 +0100

    Added method to get users allowed org list from security context

--- src/main/java/org/endeavourhealth/common/security/SecurityUtils.java
+++ src/main/java/org/endeavourhealth/common/security/SecurityUtils.java
@@ -184,4 +184,22 @@ public class SecurityUtils {
         }
         return endUser;
     }
+
+    public Set<String> getUserAllowedOrganisationIdsFromSecurityContext(SecurityContext securityContext) {
+        Set<String> orgs = new HashSet<>();
+
+        AccessToken accessToken = SecurityUtils.getToken(securityContext);
+        List<Map<String, Object>> orgGroups = (List)accessToken.getOtherClaims().getOrDefault("orgGroups", null);
+
+        if (orgGroups != null) {
+            for (Object orgGroup1 : orgGroups) {
+                Map<String, Object> orgGroup = (Map) orgGroup1;
+                String orgGroupOrganisationId = (String) orgGroup.getOrDefault("organisationId", null);
+                if (orgGroupOrganisationId != null)
+                    orgs.add(orgGroupOrganisationId);
+            }
+        }
+
+        return orgs;
+    }
 }
