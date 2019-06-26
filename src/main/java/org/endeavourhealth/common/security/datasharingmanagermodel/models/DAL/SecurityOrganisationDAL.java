bcb2768ce2a45250dbda2f91c0d442f0b8a66101
stuart <stuart@bearclawsolutions.co.uk>
2019-06-26 14:32:48 +0100

    Add to the organisation cache to allow searching by single or multiple ods codes

--- src/main/java/org/endeavourhealth/common/security/datasharingmanagermodel/models/DAL/SecurityOrganisationDAL.java
+++ src/main/java/org/endeavourhealth/common/security/datasharingmanagermodel/models/DAL/SecurityOrganisationDAL.java
@@ -120,4 +120,42 @@ public class SecurityOrganisationDAL {
 
         return orgs.subList(fromIndex, Math.min(fromIndex + pageSize, orgs.size()));
     }
+
+    public List<OrganisationEntity> getOrganisationsFromOdsList(List<String> odsCodes) throws Exception {
+        EntityManager entityManager = ConnectionManager.getDsmEntityManager();
+
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<OrganisationEntity> cq = cb.createQuery(OrganisationEntity.class);
+        Root<OrganisationEntity> rootEntry = cq.from(OrganisationEntity.class);
+
+        Predicate predicate = rootEntry.get("odsCode").in(odsCodes);
+
+        cq.where(predicate);
+        TypedQuery<OrganisationEntity> query = entityManager.createQuery(cq);
+
+        List<OrganisationEntity> ret = query.getResultList();
+
+        entityManager.close();
+
+        return ret;
+    }
+
+    public OrganisationEntity getOrganisationsFromOdsCode(String odsCode) throws Exception {
+        EntityManager entityManager = ConnectionManager.getDsmEntityManager();
+
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<OrganisationEntity> cq = cb.createQuery(OrganisationEntity.class);
+        Root<OrganisationEntity> rootEntry = cq.from(OrganisationEntity.class);
+
+        Predicate predicate = cb.equal(rootEntry.get("odsCode"), odsCode);
+
+        cq.where(predicate);
+        TypedQuery<OrganisationEntity> query = entityManager.createQuery(cq);
+
+        List<OrganisationEntity> ret = query.getResultList();
+
+        entityManager.close();
+
+        return ret.get(0);
+    }
 }
--- src/main/java/org/endeavourhealth/common/security/usermanagermodel/models/caching/OrganisationCache.java
+++ src/main/java/org/endeavourhealth/common/security/usermanagermodel/models/caching/OrganisationCache.java
@@ -4,10 +4,7 @@ package org.endeavourhealth.common.security.usermanagermodel.models.caching;
 import org.endeavourhealth.common.security.datasharingmanagermodel.models.DAL.SecurityOrganisationDAL;
 import org.endeavourhealth.common.security.datasharingmanagermodel.models.database.OrganisationEntity;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class OrganisationCache {
 
@@ -54,6 +51,59 @@ public class OrganisationCache {
 
         return organisationEntity;
 
+    }
+
+    public static OrganisationEntity getOrganisationDetailsFromOdsCode(String odsCode) throws Exception {
+
+        OrganisationEntity foundOrg = findOrgByOdsInCache(odsCode);
+        if (foundOrg == null) {
+            foundOrg = new SecurityOrganisationDAL().getOrganisationsFromOdsCode(odsCode);
+            organisationMap.put(foundOrg.getUuid(), foundOrg);
+        }
+
+        CacheManager.startScheduler();
+
+        return foundOrg;
+
+    }
+
+    public static List<OrganisationEntity> getOrganisationDetailsFromOdsCodeList(List<String> odsCodes) throws Exception {
+
+        List<OrganisationEntity> organisationEntities = new ArrayList<>();
+        List<String> missingOrgs = new ArrayList<>();
+
+        for (String odsCode : odsCodes) {
+            OrganisationEntity foundOrg = findOrgByOdsInCache(odsCode);
+            if (foundOrg == null) {
+                missingOrgs.add(odsCode);
+            } else {
+                organisationEntities.add(foundOrg);
+            }
+        }
+
+        if (missingOrgs.size() > 0) {
+            List<OrganisationEntity> entities = new SecurityOrganisationDAL().getOrganisationsFromOdsList(missingOrgs);
+
+            for (OrganisationEntity org : entities) {
+                organisationMap.put(org.getUuid(), org);
+                organisationEntities.add(org);
+            }
+        }
+        CacheManager.startScheduler();
+
+        return organisationEntities;
+
+    }
+
+    private static OrganisationEntity findOrgByOdsInCache(String odsCode) throws Exception {
+
+        for (OrganisationEntity org : organisationMap.values()) {
+            if (org.getOdsCode().toLowerCase().equals(odsCode.trim().toLowerCase())) {
+                return org;
+            }
+        }
+
+        return null;
     }
 
     public static void flushCache() throws Exception {
