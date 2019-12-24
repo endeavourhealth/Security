69e261b0752e7ca155f0793e5a721fccf80cf182
Mike Matthews <mike@factor-50.co.uk>
2019-12-24 14:45:05 +0000

    Auditing Regions

--- src/main/java/org/endeavourhealth/common/security/datasharingmanagermodel/models/database/RegionEntity.java
+++ src/main/java/org/endeavourhealth/common/security/datasharingmanagermodel/models/database/RegionEntity.java
@@ -1,8 +1,12 @@
 package org.endeavourhealth.common.security.datasharingmanagermodel.models.database;
 
+import org.endeavourhealth.common.security.datasharingmanagermodel.models.DAL.SecurityMasterMappingDAL;
+import org.endeavourhealth.common.security.datasharingmanagermodel.models.enums.MapType;
 import org.endeavourhealth.common.security.datasharingmanagermodel.models.json.JsonRegion;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "region", schema = "data_sharing_manager")
@@ -10,14 +14,45 @@ public class RegionEntity {
     private String uuid;
     private String name;
     private String description;
+    @Transient private List<String> sharingAgreements;
+    @Transient private List<String> processingAgreements;
+    @Transient private List<String> parentRegions;
+    @Transient private List<String> childRegions;
+    @Transient private List<String> organisations;
 
-    public RegionEntity() {
-    }
+
+    public RegionEntity() {    }
 
     public RegionEntity(JsonRegion region) {
+        updateFromJson(region);
+    }
+
+    public void updateFromJson(JsonRegion region) {
         this.uuid = region.getUuid();
         this.name = region.getName();
         this.description = region.getDescription();
+
+        this.sharingAgreements = new ArrayList<>();
+        region.getSharingAgreements().forEach((k, v) -> this.sharingAgreements.add(k.toString()));
+        this.processingAgreements = new ArrayList<>();
+        region.getProcessingAgreements().forEach((k, v) -> this.processingAgreements.add(k.toString()));
+        this.parentRegions = new ArrayList<>();
+        region.getParentRegions().forEach((k, v) -> this.parentRegions.add(k.toString()));
+        this.childRegions = new ArrayList<>();
+        region.getChildRegions().forEach((k, v) -> this.childRegions.add(k.toString()));
+        this.organisations = new ArrayList<>();
+        region.getOrganisations().forEach((k, v) -> this.organisations.add(k.toString()));
+    }
+
+    public void setMappingsFromDAL () throws Exception {
+        SecurityMasterMappingDAL securityMasterMappingDAL = new SecurityMasterMappingDAL();
+        Short thisMapType = MapType.REGION.getMapType();
+
+        this.setSharingAgreements(securityMasterMappingDAL.getChildMappings(this.uuid, thisMapType, MapType.DATASHARINGAGREEMENT.getMapType()));
+        this.setProcessingAgreements(securityMasterMappingDAL.getChildMappings(this.uuid, thisMapType, MapType.DATAPROCESSINGAGREEMENT.getMapType()));
+        this.setParentRegions(securityMasterMappingDAL.getParentMappings(this.uuid, MapType.CHILDREGION.getMapType(), MapType.PARENTREGION.getMapType()));
+        this.setChildRegions(securityMasterMappingDAL.getChildMappings(this.uuid, MapType.PARENTREGION.getMapType(), MapType.CHILDREGION.getMapType()));
+        this.setOrganisations(securityMasterMappingDAL.getChildMappings(this.uuid, thisMapType, MapType.ORGANISATION.getMapType()));
     }
 
     @Id
@@ -48,6 +83,56 @@ public class RegionEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    @Transient
+    public List<String> getSharingAgreements() {
+        return this.sharingAgreements;
+    }
+
+    @Transient
+    public void setSharingAgreements(List<String> sharingAgreements) {
+        this.sharingAgreements = sharingAgreements;
+    }
+
+    @Transient
+    public List<String> getProcessingAgreements() {
+        return this.processingAgreements;
+    }
+
+    @Transient
+    public void setProcessingAgreements(List<String> processingAgreements) {
+        this.processingAgreements = processingAgreements;
+    }
+
+    @Transient
+    public List<String> getParentRegions () {
+        return this.parentRegions;
+    }
+
+    @Transient
+    public void setParentRegions(List<String> parentRegions) {
+        this.parentRegions = parentRegions;
+    }
+
+    @Transient
+    public List<String> getChildRegions () {
+        return this.childRegions;
+    }
+
+    @Transient
+    public void setChildRegions(List<String> childRegions) {
+        this.childRegions = childRegions;
+    }
+
+    @Transient
+    public List<String> getOrganisations () {
+        return this.organisations;
+    }
+
+    @Transient
+    public void setOrganisations(List<String> organisations) {
+        this.organisations = organisations;
     }
 
     @Override
